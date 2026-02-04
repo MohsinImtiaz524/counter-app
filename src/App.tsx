4ab602187ef24edc8f7d0d21b9ba458fe55f0bac
MohsinImtiaz524 <mohsinimtiaz524@gmail.com>
2026-02-04 21:44:19 +0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@
 
 // -> 1 The number increases, decreases, and resets correctly when the respective buttons are clicked.
 
-// import { useState } from "react";
-// import {Button_inc, Button_dec, Button_reset, Paragraph} from "./components/Btn";
+import { useState } from "react";
+import {Button_inc, Button_dec, Button_reset, Paragraph} from "./components/Btn";
 
-// function App(){
-//   const [setCount, getCount] = useState(0);
-//   return(
-//     <>
-//     <Paragraph value={setCount}></Paragraph>
-//     <Button_inc on_Click={()=>{getCount(setCount+1)}}></Button_inc>
-//     <Button_dec on_Click={()=>{getCount(setCount-1)}}></Button_dec>
-//     <Button_reset on_Click={()=>{getCount(0)}}></Button_reset>
-//     </>
-//   );
-// }
-// export default App;
+function App(){
+  const [setCount, getCount] = useState(0);
+  return(
+    <>
+    <Paragraph value={setCount}></Paragraph>
+    <Button_inc on_Click={()=>{getCount(setCount+1)}}></Button_inc>
+    <Button_dec on_Click={()=>{getCount(setCount-1)}}></Button_dec>
+    <Button_reset on_Click={()=>{getCount(0)}}></Button_reset>
+    </>
+  );
+}
+export default App;
 
 // ---------------------------------------------------------
 
--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -1,25 +1,25 @@
 // -> 1
 
-// interface Count {
-//   value: number;
-// }
+interface Count {
+  value: number;
+}
 
-// interface btn{
-//   on_Click: ()=> void;
-// }
-// const Paragraph = ({ value }: Count) => {
-//   return <p>{value}</p>;
-// };
-// const Button_inc = ({on_Click}: btn) => {
-//   return <button onClick={on_Click} className="btn btn-primary">Increase</button>;
-// };
-// const Button_dec = ({on_Click}: btn) => {
-//   return <button onClick={on_Click} className="btn btn-secondary">Decrease</button>;
-// };
-// const Button_reset = ({on_Click}: btn) => {
-//   return <button onClick={on_Click} className="btn btn-danger">Reset</button>;
-// };
-// export { Button_inc, Button_dec, Button_reset, Paragraph };
+interface btn{
+  on_Click: ()=> void;
+}
+const Paragraph = ({ value }: Count) => {
+  return <p>{value}</p>;
+};
+const Button_inc = ({on_Click}: btn) => {
+  return <button onClick={on_Click} className="btn btn-primary">Increase</button>;
+};
+const Button_dec = ({on_Click}: btn) => {
+  return <button onClick={on_Click} className="btn btn-secondary">Decrease</button>;
+};
+const Button_reset = ({on_Click}: btn) => {
+  return <button onClick={on_Click} className="btn btn-danger">Reset</button>;
+};
+export { Button_inc, Button_dec, Button_reset, Paragraph };
 // ---------------------------------------------------------
 
 
